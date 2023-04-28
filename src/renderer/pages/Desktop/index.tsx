6a9889392daa96db71ccae48b05bda0c49fb3db4
smileluck <12386904+smileluck@users.noreply.github.com>
2023-04-28 17:45:30 +0800

    update

---
+++ src/renderer/pages/Desktop/index.tsx
@@ -0,0 +1,11 @@
+
+import style from './style.module.css'
+export default function Desktop() {
+    return (
+        <>
+            <div className={style.desktop}>
+                <div className={style.desktopWrapper}></div>
+            </div>
+        </>
+    )
+}
