a2b4d14d31a7a585a9d1aa100660aed561a622a3
smileluck <12386904+smileluck@users.noreply.github.com>
2023-05-03 16:11:11 +0800

    update desktop main

--- src/renderer/pages/Desktop/index.tsx
+++ src/renderer/pages/Desktop/index.tsx
@@ -4,7 +4,28 @@ export default function Desktop() {
     return (
         <>
             <div className={style.desktop}>
-                <div className={style.desktopWrapper}></div>
+                <div className={style.desktopWrapper}>
+                    {/* <div className='w-1/5 mx-auto px-2 py-2'>
+                        <div className=''></div>
+                    </div> */}
+                    <div className="w-4/5 box-border container mx-auto px-2 py-2 grid grid-cols-6">
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                        <div className="w-28 h-28 p-2 bg-white mx-1 my-1 rounded-md bg-opacity-75">12</div>
+                    </div>
+                </div>
             </div>
         </>
     )
